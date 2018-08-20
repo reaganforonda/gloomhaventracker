0183f86a13d6bd95e5f9abe15478228de8ccc581
reaganforonda <foronda.reagan@gmail.com>
2018-08-20 14:14:35 -0600

    created intial partyview file

---
+++ src/Components/PartyView/PartyView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class PartyView extends React.Component{
+    render (){
+        return (
+            <div className='party-view-container'>
+                
+            </div>
+        )
+    }
+}
