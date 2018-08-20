ddd5e897d90585c976067942b3b07847f745cc88
reaganforonda <foronda.reagan@gmail.com>
2018-08-20 13:05:56 -0600

    added dashboard to routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Landing from './Components/Landing/Landing';
-import Register from './Components/Register/Register'
+import Register from './Components/Register/Register';
+import Dashboard from './Components/Dashboard/Dashboard';
 
 export default (
     <Switch>
         <Route exact path='/' component={Landing}/>
         <Route path='/register' component={Register}/>
+        <Route path='/dashboard' component={Dashboard}/>
     </Switch>
 )
