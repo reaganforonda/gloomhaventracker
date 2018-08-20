6bb997b82df519524f49fadbdcacda25b3281205
reaganforonda <foronda.reagan@gmail.com>
2018-08-20 17:39:52 -0600

    function for creating party

--- server/controllers/partyController.js
+++ server/controllers/partyController.js
@@ -1,9 +1,20 @@
 const axios = require('axios');
 
 module.exports = {
-    createPary: (req, res) => {
+    createParty: (req, res) => {
         const db = req.app.get('db');
+        const {userID, name, location, gold} = req.body;
 
-        
+        db.CREATE_PARTY([userID, name, location, gold]).then((result) => {
+            console.log(result); //TODO: REMOVE
+            res.status(200).send(result);
+        }).catch((err) => {
+            console.log(`Server error while attempting to create party: ${err}`);
+            res.sendStatus(500);
+        })
+    },
+
+    updateParty: (req, res) => {
+        // TODO:
     }
 }
