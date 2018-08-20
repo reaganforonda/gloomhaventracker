660681f9a295d52f77a5dc7bf09278b6bb3c3504
reaganforonda <foronda.reagan@gmail.com>
2018-08-20 17:14:26 -0600

    created function for header btn click

--- src/Components/PartyView/PartyView.js
+++ src/Components/PartyView/PartyView.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import {withRouter} from 'react-router-dom';
+import {withRouter, Switch, Route} from 'react-router-dom';
+import PartyCreationForm from './PartyCreationForm';
 
 export class PartyView extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state={}
+
+        this.handlePartyBtnClick = this.handlePartyBtnClick.bind(this);
+    }
+
+    handlePartyBtnClick(){
+        this.props.history.push('/dashboard/party/add')
+    }
+
     render (){
         return (
             <div className='party-view-container'>
-                <Header headerTitle={"Party"}/>
-                    <main className='party-view-main'>
-                        Party View View
-                    </main>
+                <Header clickBtn={this.handlePartyBtnClick} headerTitle={"Party"} btn={'Create'}/>
+                <main className='party-view-main'>
+                    <Switch>
+                        <Route path='/dashboard/party/add' component={PartyCreationForm}/>
+                    </Switch>
+                </main>
                 <Footer/>
             </div>
         )
