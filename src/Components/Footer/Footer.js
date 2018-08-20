940d9c5a490be7b634006a67e96af3efb641d841
reaganforonda <foronda.reagan@gmail.com>
2018-08-20 13:08:40 -0600

    added placeholder links

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import {withRouter} from 'react-router-dom';
+import {withRouter, Link} from 'react-router-dom';
 
 export class Footer extends React.Component{
     render(){
         return (
             <footer className='footer'>
-                Footer
+                <div className='footer-section'>
+                    <Link to='/dashboard'>Home</Link>
+                </div>
+                <div className='footer-section'>
+                    Party
+                </div>
+                <div className='footer-section'>
+                    Characters
+                </div>
+                <div className='footer-section'>
+                    Combat
+                </div>
             </footer>
         )
     }
