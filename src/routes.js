d1f888a67d1be4b890ad927f134e7759b7e5a442
reaganforonda <foronda.reagan@gmail.com>
2018-08-20 12:00:00 -0600

    added register route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Landing from './Components/Landing/Landing';
+import Register from './Components/Register/Register'
 
 export default (
     <Switch>
         <Route exact path='/' component={Landing}/>
+        <Route path='/register' component={Register}/>
     </Switch>
 )
