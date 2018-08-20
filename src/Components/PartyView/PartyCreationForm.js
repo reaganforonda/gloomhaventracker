8b2522265018227a9988aa0559e9f2f3aff01a9a
reaganforonda <foronda.reagan@gmail.com>
2018-08-20 17:24:23 -0600

    work on add party form

--- src/Components/PartyView/PartyCreationForm.js
+++ src/Components/PartyView/PartyCreationForm.js
@@ -5,17 +5,35 @@ export class PartyCreationForm extends React.Component{
     constructor(props){
         super(props);
 
-        this.state={}
+        this.state={
+            name: '',
+            location: '',
+            reputation: ''
+        }
+    }
+
+    handleInputChange(e) {
+        this.setState({[e.target.name] : e.target.value})
     }
 
     render(){
         return (
             <form className='player-create-form'>
                 <div className='player-create-form-row'>
+                    
                 </div>
                 <div className='player-create-form-row'>
-                    Name: <input/>
+                    Name: <input type='text' name='name'/>
                 </div>
+                <div className='player-create-form-row'>
+                    Location: <input type='text' name='location' />
+                </div>
+                <div className='player-create-form-row'>
+                    Reputation: <input type='number' name='reputation'/>
+                </div>
+                <div className='player-create-form-row'>
+                    <input type='submit' placeholder='Create Party'/>
+                </div>                
             </form>
         )
     }
