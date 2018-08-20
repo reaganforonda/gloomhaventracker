c805be470fc09886c6d71f8329f6b8d3a6bf640f
reaganforonda <foronda.reagan@gmail.com>
2018-08-20 16:13:29 -0600

    edit Pary View

--- src/Components/PartyView/PartyView.js
+++ src/Components/PartyView/PartyView.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import {withRouter} from 'react-router-dom';
 
 export class PartyView extends React.Component{
     render (){
         return (
             <div className='party-view-container'>
-                
+                <Header headerTitle={"Party"}/>
+                    <main className='party-view-main'>
+                        Party View View
+                    </main>
+                <Footer/>
             </div>
         )
     }
 }
+
+export default withRouter(PartyView);
