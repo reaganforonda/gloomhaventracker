49a4debea631afd1328ecd7212f96174aa76e5da
reaganforonda <foronda.reagan@gmail.com>
2018-08-20 11:59:46 -0600

    created register file

---
+++ src/Components/Register/Register.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {withRouter} from 'react-router-dom';
+
+export class Register extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state={}
+    }
+
+    render(){
+        return (
+            <div className='register-container'>
+                
+            </div>
+        )
+    }
+}
+
+export default withRouter(Register);
