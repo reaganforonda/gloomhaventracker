15e0fae4b72dc07406841be1d2c1408a3b3df9f4
reaganforonda <foronda.reagan@gmail.com>
2018-08-19 22:15:48 -0600

    moved server file and folder

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-barser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
