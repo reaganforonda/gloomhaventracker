3c6fa276a98f29829a58c7c27697b12b198f227d
reaganforonda <foronda.reagan@gmail.com>
2018-08-20 18:41:22 -0600

    create sidemenu

---
+++ src/Components/SideMenu/SideMenu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SideMenu(props){
+    return (
+        <div className='side-menu-container'>
+            <ul className='side-menu'>
+                <li className='side-menu-item'>Logout</li>
+            </ul>
+        </div>
+    )
+}
