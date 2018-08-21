c027a4b51310e7c40c3b4d95c67b2e2b4259994f
reaganforonda <foronda.reagan@gmail.com>
2018-08-21 08:29:59 -0600

    removed console.log

--- server/controllers/partyController.js
+++ server/controllers/partyController.js
@@ -4,6 +4,7 @@ module.exports = {
     createParty: (req, res) => {
         const db = req.app.get('db');
         const {userID, name, location, gold} = req.body;
+        console.log(req.body);
 
         db.CREATE_PARTY([userID, name, location, gold]).then((result) => {
             console.log(result); //TODO: REMOVE
