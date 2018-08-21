222e5445f46421fb69c87ec21fe2a29af57dd20b
reaganforonda <foronda.reagan@gmail.com>
2018-08-20 18:02:40 -0600

    work on login form

---
+++ src/Components/LoginForm/LoginForm.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {setActiveUser} from '../../ducks/partyReducer';
+
+export class LoginForm extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            userName : '',
+            password : ''
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.submitSignInForm = this.submitSignInForm.bind(this);
+        this.resetForm = this.resetForm.bind(this);
+    }
+
+    handleInputChange(e) {
+        this.setState({[e.target.name] : e.target.value})
+    }
+
+    submitSignInForm(e) {
+        e.preventDefault();
+    }
+
+    resetForm(){
+        this.setState({
+            userName : '',
+            password : ''
+        })
+    }
+
+    render(){
+        return (
+            <form className='login-form'>
+                <div className='login-form-row'>
+                    User Name: <input onChange={(e) => this.handleInputChange(e)} name='userName' type='text' />
+                </div>
+                <div className='login-form-row'>
+                    Password: <input onChange={(e) => this.handleInputChange(e)} name='password' type='password'/>
+                </div>
+                <div className='login-form-row'>
+                    <input onClick={(e)=>this.submitSignInForm(e)}type='submit' placeholder='Submit'/>
+                </div>
+            </form>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        user: state.userReducer.user
+    }
+}
+
+export default connect(mapStateToProps, {setActiveUser})(withRouter(LoginForm))
