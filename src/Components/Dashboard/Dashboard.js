d74e88ea1ab37377a5e46ce598dc19f6c4a7e03a
reaganforonda <foronda.reagan@gmail.com>
2018-08-21 13:02:19 -0600

    added auth check to dashboard

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -2,12 +2,22 @@ import React from 'react';
 import {Switch, Route, withRouter} from 'react-router-dom';
 import PartyView from '../PartyView/PartyView';
 import { CampaignView } from '../CampaignView/CampaignView';
+import axios from 'axios';
 
 export class Dashboard extends React.Component{
     constructor(props){
         super(props);
 
         this.state={}
+    }
+    
+    componentDidMount = async() => {
+        await axios.get('/api/auth/me').then((user)=> {
+            console.log('Logged In');
+        }).catch((err) => {
+            console.log(err); //TODO: 
+            this.props.history.push('/')
+        })
     }
     render(){
         return(
