cbab0028b8a5a08e1fe673ab0f4e254fa99ce2cb
reaganforonda <foronda.reagan@gmail.com>
2018-08-21 15:24:01 -0600

    added values for achievements

--- db/SEED.sql
+++ db/SEED.sql
@@ -7,3 +7,183 @@ INSERT INTO achievements
 (achievement_name)
 VALUES
 ('A Map to Treasure');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('Across the Divide');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('Ancient Technology');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('Annihilation of Order');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('Artifact Lost');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('Artifact Recovered');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('Artifact:Cleansed');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('City Rule: Demonic');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('City Rule: Economic');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('City Rule: Militaristic');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('Dark Bounty');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('Edge of Darkness');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('End of Corruption');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('End of Gloom');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('End of the invasion');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('First Steps');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('Follwing Clues');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('Jekserah''s Plans');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('Redthorn''s Aid');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('Stonebreaker''s Censer');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('The Dead Invade');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('The Drake Slain');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('The Drake''s Command');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('The Drake''s Treasure');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('The Merchant Flees');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('The Poison''s Source');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('The Power of Enchantment');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('The Rift Neutralized');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('The Scepter and the Voice');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('The Voice Freed');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('The Voice Silenced');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('The Voice''s Command');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('The Voice''s Treasure');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('Through the Nest');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('Through the Ruins');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('Through the Trench');
+
+INSERT INTO achievements
+(achievement_name)
+VALUES
+('Water-Breathing');
