5e7ace9ed66ab0ddb40b44a0fbc4ab064732830b
reaganforonda <foronda.reagan@gmail.com>
2018-08-21 15:23:37 -0600

    added shop price modifier

--- db/CREATE_PARTY.sql
+++ db/CREATE_PARTY.sql
@@ -1,4 +1,4 @@
 INSERT INTO parties
-(user_id, party_name, party_location, party_gold, reputation)
+(user_id, party_name, party_location, party_gold, reputation, price_mod)
 VALUES
-($1, $2, $3, $4, $5)
+($1, $2, $3, $4, $5, $6)
