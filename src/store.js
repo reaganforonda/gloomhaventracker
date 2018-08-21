54285a529a57a2542e48f37cf55d86d013822cfb
reaganforonda <foronda.reagan@gmail.com>
2018-08-21 13:25:33 -0600

    add party reducer to store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
 import userReducer from './ducks/userReducer';
+import partyReducer from './ducks/partyReducer';
 
 const reducers = {
-    userReducer: userReducer
+    userReducer: userReducer,
+    partyReducer : partyReducer
 }
 
 let middleware = promiseMiddleware();
