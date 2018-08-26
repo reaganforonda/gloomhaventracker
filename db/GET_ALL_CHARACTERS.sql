49bc1fe58655cbc8ca2cdd26cf01794aa1843655
reaganforonda <foronda.reagan@gmail.com>
2018-08-26 07:59:49 -0600

    update sql file

--- db/GET_ALL_CHARACTERS.sql
+++ db/GET_ALL_CHARACTERS.sql
@@ -1,3 +1,5 @@
-SELECT *
+SELECT character_id, user_id, party_id, characters.class_id, character_name, experience, level, gold, retired, items, classes.class_name as class_name
 FROM characters
-WHERE user_id = $1
+JOIN classes
+ON classes.class_id = characters.class_id
+WHERE characters.user_id = $1
