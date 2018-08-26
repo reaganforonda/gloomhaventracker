ad036922d930d671f5f0ff12ea2cfd1587620abf
reaganforonda <foronda.reagan@gmail.com>
2018-08-25 22:08:52 -0600

    added characterreducer

--- src/store.js
+++ src/store.js
@@ -3,11 +3,13 @@ import promiseMiddleware from 'redux-promise-middleware';
 import userReducer from './ducks/userReducer';
 import partyReducer from './ducks/partyReducer';
 import dataReducer from './ducks/dataReducer';
+import characterReducer from './ducks/characterReducer';
 
 const reducers = {
     userReducer: userReducer,
     partyReducer : partyReducer,
-    dataReducer: dataReducer
+    dataReducer: dataReducer,
+    characterReducer : characterReducer
 }
 
 let middleware = promiseMiddleware();
