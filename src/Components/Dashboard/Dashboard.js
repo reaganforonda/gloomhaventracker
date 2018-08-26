3d684b622c7ffb7b8e566e0d9b300b09de484db7
reaganforonda <foronda.reagan@gmail.com>
2018-08-25 18:40:00 -0600

    fix bug in route

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Switch, Route, withRouter} from 'react-router-dom';
 import PartyView from '../PartyView/PartyView';
-import { CampaignView } from '../CampaignView/CampaignView';
+import CampaignView from '../CampaignView/CampaignView';
 import axios from 'axios';
 import {connect} from 'react-redux';
 import {getAllParties} from '../../ducks/partyReducer';
+import CharacterView from '../Character/CharacterView';
 
 export class Dashboard extends React.Component{
     constructor(props){
@@ -19,7 +20,8 @@ export class Dashboard extends React.Component{
                 <main className='dashboard-main'>
                     <Switch>
                         <Route path='/dashboard/party' component={PartyView}/>
-                        <Route path='/dashboard' component={CampaignView}/>
+                        <Route exact path='/dashboard' component={CampaignView}/>
+                        <Route path='/dashboard/characters' component={CharacterView}/>
                     </Switch>
                 </main>
             </div>
