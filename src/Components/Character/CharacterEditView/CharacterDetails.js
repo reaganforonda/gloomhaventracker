c20249c1023968508f7f98375a0c36d2cdac73c3
reaganforonda <foronda.reagan@gmail.com>
2018-08-26 17:53:47 -0600

    work on character details view

--- src/Components/Character/CharacterEditView/CharacterDetails.js
+++ src/Components/Character/CharacterEditView/CharacterDetails.js
@@ -8,16 +8,52 @@ export class CharacterDetails extends React.Component {
         super(props);
 
         this.state = {
+            characterName: '',
+            level: '',
+            characterClass: ''
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    static getDerivedStateFromProps(props, state){
+        if(props.character){
+            return {
+                characterName : props.character.character_name,
+                level: props.character.level,
+                characterClass: props.character.class_name
+            }
         }
     }
 
+    handleInputChange(e) {
+        this.setState({[e.target.name] : e.tarvet.value})
+    };
+
     render(){
-        console.log(this.props.character);
         return (
             <div className='character-details-container'>
                 <main className='character-details-main'>
                     <div className='character-details-main-row'>
-                        Name: <input/>
+                        <p>Name</p> <input value={this.state.characterName} name='characterName' type='text' 
+                            onChange={(e) => this.handleInputChange(e)}/>
+                    </div>
+                    <div className='character-details-main-row'>
+                        <p>Class</p>
+                        <input value={this.state.characterClass} name='characterClass' type='text' disabled/>
+                    </div>
+                    <div className='character-details-main-row'>
+                        <p>Level</p>
+                        <input disabled value={this.state.level} name='level' type='number'/>
+                    </div>
+                    <div className='character-details-main-row'>
+                        <p>Experience</p>
+                    </div>
+                    <div className='character-details-main-row'>
+                        <p>Gold</p>
+                    </div>
+                    <div className='character-details-main-row'>
+                        <p>Notes</p>
                     </div>
                 </main>
             </div>
