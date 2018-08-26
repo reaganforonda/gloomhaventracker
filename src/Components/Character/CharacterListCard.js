e30277accd172e0862220e9171186ce89a311f3a
reaganforonda <foronda.reagan@gmail.com>
2018-08-26 08:14:30 -0600

    create characterlistcard file

---
+++ src/Components/Character/CharacterListCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+export default function CharacterListCard(props) {
+
+    let characterRow = []
+    
+    if(props.characters) {
+        characterRow = props.characters.map((value, index) => {
+            return (
+                <div key={value.character_id + index} className='character-list-card-row'>
+                    <div className='character-list-card-col'>
+                        {value.character_name}
+                    </div>
+                    <div className='character-list-card-col'>
+                        {value.class_name}
+                    </div>
+                    <div className='character-list-card-col'>
+                        {value.level}
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    return (
+        <div className='character-list-card'>
+            {characterRow}
+        </div>
+    )
+}
