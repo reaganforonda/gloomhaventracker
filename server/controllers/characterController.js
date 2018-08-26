934fff26b1cc1b5bf40d7e784d5bacaac0a47e22
reaganforonda <foronda.reagan@gmail.com>
2018-08-26 00:13:10 -0600

    add character reducer and controller files

--- server/controllers/characterController.js
+++ server/controllers/characterController.js
@@ -4,5 +4,19 @@ module.exports = {
     createCharacter: (req, res) => {
         const db = req.app.get('db');
 
+        const {userID, characterName, characterClass} = req.body;
+
+        db.CREATE_NEW_CHARACTER([userID, characterName, characterClass]).then((result) => {
+            res.status(200).send(result);
+        }).catch((err) => {
+            console.log(`Server error while attempting to create new character: ${err}`);
+            res.sendStatus(500);
+        })
+    },
+
+    getAllCharacters: (req, res) => {
+        const db = req.app.get('db');
+
+        const {userID} = rq.params;
     }
 }
--- src/ducks/characterReducer.js
+++ src/ducks/characterReducer.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
 const INITIAL_STATE = {
-
+    character: [],
 }
 
 
 export default function characterReducer(state=INITIAL_STATE, action) {
     switch(action.type) {
 
+        
 
         default:
             return state;
