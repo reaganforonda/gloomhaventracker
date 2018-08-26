93c71e61ff143bdc1cf2d135ce89fd29bd4fc390
reaganforonda <foronda.reagan@gmail.com>
2018-08-26 08:58:38 -0600

    add sql to get selected character

---
+++ db/GET_SELECT_CHARACTER.sql
@@ -0,0 +1,5 @@
+SELECT character_id, user_id, party_id, characters.class_id, character_name, experience, level, gold, retired, items, classes.class_name as class_name
+FROM characters
+JOIN classes
+ON classes.class_id = characters.class_id
+WHERE characters.character_id = $1
