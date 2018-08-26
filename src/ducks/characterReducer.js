5296be451822b9e011b23ffe446e0c36b079c0e3
reaganforonda <foronda.reagan@gmail.com>
2018-08-25 22:08:09 -0600

    create character reducer

---
+++ src/ducks/characterReducer.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const INITIAL_STATE = {
+
+}
+
+
+export default function characterReducer(state=INITIAL_STATE, action) {
+    switch(action.type) {
+
+
+        default:
+            return state;
+    }
+}
