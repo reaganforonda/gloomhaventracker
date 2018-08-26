557e45a988de438e8b1cf280db1d62dd952a519f
reaganforonda <foronda.reagan@gmail.com>
2018-08-25 18:35:25 -0600

    added template code for character edit file

---
+++ src/Components/Character/CharacterEdit.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
+import axios from 'axios';
+
+
+export class CharacterEdit extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state={};
+    }
+
+    render(){
+        return (
+            <div className='character-edit-container'>
+                
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        user: state.userReducer.user
+    }
+}
+
+export default connect(mapStateToProps, {})(withRouter(CharacterEdit));
+
