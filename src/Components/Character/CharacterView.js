1a2a77afbf71cc501e3424fb92bf42364e4d3f2b
reaganforonda <foronda.reagan@gmail.com>
2018-08-25 18:35:31 -0600

    added routes

--- src/Components/Character/CharacterView.js
+++ src/Components/Character/CharacterView.js
@@ -3,6 +3,8 @@ import {withRouter, Switch, Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 import Header from '../Header/Header';
 import CharacterList from './CharacterList';
+import CreateCharacterForm from './CreateCharacterForm';
+import CharacterEdit from './CharacterEdit';
 
 export class CharacterView extends React.Component{
     render(){
@@ -12,7 +14,8 @@ export class CharacterView extends React.Component{
                 <main className='character-view-main'>
                     <Switch>
                         <Route exact path='/dashboard/characters' component={CharacterList}/>
-                        <Route path='/dashboard/characteres/add' component={}/>
+                        <Route path='/dashboard/characteres/add' component={CreateCharacterForm}/>
+                        <Route path='/dashboard/characters/edit' component={CharacterEdit}/>
                     </Switch>
                 </main>
             </div>
