aef42c105912bdce181bfec088231c3aa0564f22
reaganforonda <foronda.reagan@gmail.com>
2018-08-27 16:05:10 -0600

    work on check marks

--- src/Components/Perks/CheckMarks.js
+++ src/Components/Perks/CheckMarks.js
@@ -9,24 +9,24 @@ export class CheckMarks extends React.Component {
         super(props);
 
         this.state={
-            check1:'',
-            check2:'',
-            check3: '',
-            check4: '',
-            check5: '',
-            check6: '',
-            check7: '',
-            check8: '',
-            check9: '',
-            check10: '',
-            check11: '',
-            check12: '',
-            check13: '',
-            check14: '',
-            check15: '',
-            check16: '',
-            check17: '',
-            check18: ''
+            check1: false,
+            check2: false,
+            check3: false,
+            check4: false,
+            check5: false,
+            check6: false,
+            check7: false,
+            check8: false,
+            check9: false,
+            check10: false,
+            check11: false,
+            check12: false,
+            check13: false,
+            check14: false,
+            check15: false,
+            check16: false,
+            check17: false,
+            check18: false
         }
 
         this.handleCheckSelect = this.handleCheckSelect.bind(this);
@@ -34,7 +34,7 @@ export class CheckMarks extends React.Component {
     }
 
     handleCheckSelect(e) {
-        
+        this.setState({[e.target.name] : e.target.checked})
     }
 
     handleSave(e) {
@@ -42,7 +42,23 @@ export class CheckMarks extends React.Component {
 
         let checks={
             check1: this.state.check1,
-            check2: this.state.check2
+            check2: this.state.check2,
+            check3: this.state.check3,
+            check4: this.state.check4,
+            check5: this.state.check5,
+            check6: this.state.check6,
+            check7: this.state.check7,
+            check8: this.state.check8,
+            check9: this.state.check9,
+            check10: this.state.check10,
+            check11: this.state.check11,
+            check12: this.state.check12,
+            check13: this.state.check13,
+            check14: this.state.check14,
+            check15: this.state.check15,
+            check16: this.state.check16,
+            check17: this.state.check17,
+            check18: this.state.check18
         }
 
         console.log(checks);
@@ -53,36 +69,36 @@ export class CheckMarks extends React.Component {
             <div className='checkmark-container'>
                 <div className='checkmark-row'>
                     <div className='checkmark-box'>
-                        <input name='check1' type='checkbox'/>
-                        <input name='check2' type='checkbox'/>
-                        <input name='check3' type='checkbox'/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check1' type='checkbox' checked={this.state.check1}/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check2' type='checkbox' checked={this.state.check2}/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check3' type='checkbox' checked={this.state.check3}/>
                     </div>
                     <div className='checkmark-box'>
-                        <input name='check4' type='checkbox'/>
-                        <input name='check5' type='checkbox'/>
-                        <input name='check6' type='checkbox'/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check4' type='checkbox' checked={this.state.check4}/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check5' type='checkbox' checked={this.state.check5}/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check6' type='checkbox' checked={this.state.check6}/>
                     </div>
                     <div className='checkmark-box'>
-                        <input name='check7' type='checkbox'/>
-                        <input name='check8' type='checkbox'/>
-                        <input name='check9' type='checkbox'/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check7' type='checkbox' checked={this.state.check7}/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check8' type='checkbox' checked={this.state.check8}/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check9' type='checkbox' checked={this.state.check9}/>
                     </div>
                 </div>
                 <div className='checkmark-row'>
                     <div className='checkmark-box'>
-                        <input name='check10' type='checkbox'/>
-                        <input name='check11' type='checkbox'/>
-                        <input name='check12' type='checkbox'/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check10' type='checkbox' checked={this.state.check10}/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check11' type='checkbox' checked={this.state.check11}/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check12' type='checkbox' checked={this.state.check12}/>
                     </div>
                     <div className='checkmark-box'>
-                        <input name='check13' type='checkbox'/>
-                        <input name='check14' type='checkbox'/>
-                        <input name='check15' type='checkbox'/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check13' type='checkbox' checked={this.state.check13}/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check14' type='checkbox' checked={this.state.check14}/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check15' type='checkbox' checked={this.state.check15}/>
                     </div>
                     <div className='checkmark-box'>
-                        <input name='check16' type='checkbox'/>
-                        <input name='check17' type='checkbox'/>
-                        <input name='check18' type='checkbox'/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check16' type='checkbox' checked={this.state.check16}/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check17' type='checkbox' checked={this.state.check17}/>
+                        <input onChange={(e)=>this.handleCheckSelect(e)} name='check18' type='checkbox' checked={this.state.check18}/>
                     </div>
                 </div>
                 <button onClick={(e)=>this.handleSave(e)}>Save</button>
