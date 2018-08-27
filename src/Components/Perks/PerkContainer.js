64b3f2148c3410eacb9f3cfda5e7a4cef5eee0d2
reaganforonda <foronda.reagan@gmail.com>
2018-08-27 15:13:10 -0600

    change to class component

---
+++ src/Components/Perks/PerkContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+
+export class PerkContainer extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state={}
+    }
+
+    render(){
+        return (
+            <div className='perk-container'>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        user: state.userReducer.user
+    }
+}
+
+export default connect(mapStateToProps, {})(withRouter(PerkContainer))
