bd935ccb6bace1a7499308785529f6e7736f59c7
reaganforonda <foronda.reagan@gmail.com>
2018-08-27 15:13:18 -0600

    work on check marks

--- src/Components/Character/CharacterEditView/CharacterPerks.js
+++ src/Components/Character/CharacterEditView/CharacterPerks.js
@@ -2,18 +2,22 @@ import React from 'react';
 import {withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import CheckMarks from '../../Perks/CheckMarks';
+import PerkContainer from '../../Perks/PerkContainer';
 
 export class CharacterPerks extends React.Component{
     constructor(props) {
         super(props);
 
-        this.state={}
+        this.state={
+
+        }
     }
 
     render(){
         return (
             <div className='character-perks-container'>
                 <CheckMarks/>
+                <PerkContainer/>
             </div>
         )
     }
--- src/Components/Perks/CheckMarks.js
+++ src/Components/Perks/CheckMarks.js
@@ -1,42 +1,84 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
 
-export default function CheckMarks(props) {
-    return (
-        <div className='checkmark-container'>
-            <div className='checkmark-row'>
-                <div className='checkmark-box'>
-                    <input type='checkbox'/>
-                    <input type='checkbox'/>
-                    <input type='checkbox'/>
+
+export class CheckMarks extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state={
+            check1:'',
+            check2:''
+        }
+
+        this.handleCheckSelect = this.handleCheckSelect.bind(this);
+        this.handleSave = this.handleSave.bind(this);
+    }
+
+    handleCheckSelect(e) {
+        
+    }
+
+    handleSave(e) {
+        e.preventDefault();
+
+        let checks={
+            check1: this.state.check1,
+            check2: this.state.check2
+        }
+
+        console.log(checks);
+    }
+
+    render(){
+        return (
+            <div className='checkmark-container'>
+                <div className='checkmark-row'>
+                    <div className='checkmark-box'>
+                        <input type='checkbox'/>
+                        <input type='checkbox'/>
+                        <input type='checkbox'/>
+                    </div>
+                    <div className='checkmark-box'>
+                        <input type='checkbox'/>
+                        <input type='checkbox'/>
+                        <input type='checkbox'/>
+                    </div>
+                    <div className='checkmark-box'>
+                        <input type='checkbox'/>
+                        <input type='checkbox'/>
+                        <input type='checkbox'/>
+                    </div>
                 </div>
-                <div className='checkmark-box'>
-                    <input type='checkbox'/>
-                    <input type='checkbox'/>
-                    <input type='checkbox'/>
+                <div className='checkmark-row'>
+                    <div className='checkmark-box'>
+                        <input type='checkbox'/>
+                        <input type='checkbox'/>
+                        <input type='checkbox'/>
+                    </div>
+                    <div className='checkmark-box'>
+                        <input type='checkbox'/>
+                        <input type='checkbox'/>
+                        <input type='checkbox'/>
+                    </div>
+                    <div className='checkmark-box'>
+                        <input type='checkbox'/>
+                        <input type='checkbox'/>
+                        <input type='checkbox'/>
+                    </div>
                 </div>
-                <div className='checkmark-box'>
-                    <input type='checkbox'/>
-                    <input type='checkbox'/>
-                    <input type='checkbox'/>
-                </div>
+                <button onClick={(e)=>this.handleSave(e)}>Save</button>
             </div>
-            <div className='checkmark-row'>
-                <div className='checkmark-box'>
-                    <input type='checkbox'/>
-                    <input type='checkbox'/>
-                    <input type='checkbox'/>
-                </div>
-                <div className='checkmark-box'>
-                    <input type='checkbox'/>
-                    <input type='checkbox'/>
-                    <input type='checkbox'/>
-                </div>
-                <div className='checkmark-box'>
-                    <input type='checkbox'/>
-                    <input type='checkbox'/>
-                    <input type='checkbox'/>
-                </div>
-            </div>
-        </div>
-    )
+        )
+    }
 }
+
+function mapStateToProps(state) {
+    return {
+        user: state.userReducer.state
+    }
+}
+
+export default connect(mapStateToProps, {})(withRouter(CheckMarks));
