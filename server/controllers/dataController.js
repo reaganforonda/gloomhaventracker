fe4eab6ee6c2f59c095ebf83c030a9ec3f0c2061
reaganforonda <foronda.reagan@gmail.com>
2018-08-27 13:22:08 -0600

    added getcharacterperk function

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -20,5 +20,16 @@ module.exports = {
             console.log(`Server error while attempting to retrieve classes: ${err}`)
             res.sendStatus(500);
         })
-    }
+    },
+
+    getCharacterPerks: (req, res) => {
+        const db = req.app.get('db');
+
+        db.GET_CLASS_PERKS([]).then((result) => {
+            res.status(200).send(result)
+        }).catch((err) => {
+            console.log(`Server error while attempting to retrieve perks: ${err}`);
+            res.sendStatus(500);
+        })
+    },
 }
