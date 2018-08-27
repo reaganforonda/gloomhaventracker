11b567850d8be492d8de419addc65f72b09614aa
reaganforonda <foronda.reagan@gmail.com>
2018-08-26 21:12:20 -0600

    udpate update character function

--- server/controllers/characterController.js
+++ server/controllers/characterController.js
@@ -43,9 +43,9 @@ module.exports = {
     updateCharacter: (req, res) => {
         const db = req.app.get('db');
 
-        const {characterName, level, exp} = req.body;
+        const {characterName, level, exp, gold} = req.body;
 
-        db.UPDATE_CHARACTER([characterName, level, exp]).then((result) => {
+        db.UPDATE_CHARACTER([characterName, level, exp, gold]).then((result) => {
             res.status(200).send(result);
         }).catch((err) => {
             console.log(`Server error while attempting to update character: ${err}`);
