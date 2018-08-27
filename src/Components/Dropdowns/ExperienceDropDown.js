1c40e48871867318ef08aee542b07f21a18d0010
reaganforonda <foronda.reagan@gmail.com>
2018-08-26 18:14:08 -0600

    fix default value

--- src/Components/Dropdowns/ExperienceDropDown.js
+++ src/Components/Dropdowns/ExperienceDropDown.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function ExperienceDropDown(props) {
+export default function ExperienceDropDown(props) {
     let exp = []
 
     for(var i =1; i <= 9; i ++) {
@@ -8,10 +8,10 @@ export function ExperienceDropDown(props) {
     }
 
     let options = exp.map((value, index) => {
-        return <option key={valud + index} value={value}>{value}</option>
+        return <option key={value + index} value={value}>{value}</option>
     })
     return (
-        <select onChange={(e) => handleSelect(e)}>
+        <select onChange={(e) => props.handleExpSelect(e)} defaultValue={props.exp}>
             {options}
         </select>
     )
