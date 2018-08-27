ff172ccc8b4d4436b0c1f9847b7dc1df21627c58
reaganforonda <foronda.reagan@gmail.com>
2018-08-26 19:20:53 -0600

    loading spinner

---
+++ src/Components/Loading/LoadingSpinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function LoadingSpinner(props){
+    return (
+        <div className="lds-ring">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
