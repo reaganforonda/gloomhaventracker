b80278ce2e90e69fe251140227c3809cd3297f9a
reaganforonda <foronda.reagan@gmail.com>
2018-08-27 11:34:15 -0600

    created character perks view

---
+++ src/Components/Character/CharacterEditView/CharacterPerks.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+
+
+export class CharacterPerks extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state={}
+    }
+
+    render(){
+        return (
+            <div className='character-perks-container'>
+                
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        user: state.userReducer.user,
+        character : state.characterReducer.character
+    }
+}
+
+export default connect(mapStateToProps, {})(withRouter(CharacterPerks));
