0d1da6c274d80f48f011a65e96dce86e00c962f0
reaganforonda <foronda.reagan@gmail.com>
2018-08-27 13:31:53 -0600

    added getclassperks function to reducer

--- src/ducks/dataReducer.js
+++ src/ducks/dataReducer.js
@@ -2,11 +2,24 @@ import axios from 'axios';
 
 const INITIAL_STATE ={
     achievements:[],
-    classes: []
+    classes: [],
+    classPerks: []
 }
 
 const GET_ALL_ACHIEV = "GET_ALL_ACHIEV";
 const GET_ALL_CLASSES = "GET_ALL_CLASSES";
+const GET_CLASS_PERKS = 'GET_CLASS_PERKS';
+
+export function getClassPerks(classID) {
+    let perks = axios.get(`/api/data/perks/${classID}`).then((result) => {
+        return result.data
+    })
+
+    return {
+        type: GET_CLASS_PERKS,
+        payload: perks
+    }
+}
 
 export function getAllAchievs(){
     let achievs = axios.get('/api/data/achievments').then((result) =>{
@@ -43,7 +56,13 @@ export default function dataReducer(state=INITIAL_STATE, action){
             return "Loading";
 
         case GET_ALL_CLASSES + "_FULFILLED":
-            return Object.assign({}, state, {classes : action.payload})
+            return Object.assign({}, state, {classes : action.payload});
+
+        case GET_CLASS_PERKS + "_PENDING":
+            return "Loading";
+        
+        case GET_CLASS_PERKS + "_FULFILLED":
+            return Object.assign({}, state, {classPerks : action.payload});
 
         default:
             return state;
