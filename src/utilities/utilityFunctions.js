9837e68b1b4438cdd47030723d09096c3100f8dc
reaganforonda <foronda.reagan@gmail.com>
2018-08-27 22:16:24 -0600

    work on util functions

---
+++ src/utilities/utilityFunctions.js
@@ -0,0 +1,73 @@
+module.export = {
+    defaultPerk(classID) {
+        switch(classID){
+            case 1:
+                return (
+                    {
+                        "perkCheck1": false,
+                        "perkCheck2": false,
+                        "perkCheck3A": false,
+                        "perkCheck3B": false,
+                        "perkCheck4": false,
+                        "perkCheck5A":false,
+                        "perkCheck5B":false,
+                        "perkCheck6":false,
+                        "perkCheck7A":false,
+                        "perkCheck7B":false,
+                        "perkCheck8":false,
+                        "perkCheck9A":false,
+                        "perkCheck9B":false,
+                        "perkCheck10":false,
+                        "perkCheck11":false
+                    }
+                )
+
+            // Cragheart
+            case 2:
+                return (
+                    {
+                        "perkCheck1":false,
+                        "perkCheck2A":false,
+                        "perkCheck2B":false,
+                        "perkCheck2C":false,
+                        "perkCheck3":false,
+                        "perkCheck4A":false,
+                        "perkCheck4B":false,
+                        "perkCheck5A":false,
+                        "perkCheck5B":false,
+                        "perkCheck6":false,
+                        "perkCheck7A":false,
+                        "perkCheck7B":false,
+                        "perkCheck8":false,
+                        "perkCheck9":false,
+                        "perkCheck10":false,
+                    }
+                )
+            case 3:
+                return (
+                    {
+
+                    }
+                )
+            case 4:
+                return (
+                    {
+
+                    }
+                )
+            case 5:
+                return (
+                    {
+
+                    }
+                )
+            case 6:
+                return (
+                    {
+
+                    }
+                )
+        }
+
+    }
+}
