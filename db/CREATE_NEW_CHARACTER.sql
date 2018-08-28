23ae2dfc4b213473da2ece2c6d4ccbe934fe67ac
reaganforonda <foronda.reagan@gmail.com>
2018-08-27 18:31:58 -0600

    update to add default checkmarks and perks

--- db/CREATE_NEW_CHARACTER.sql
+++ db/CREATE_NEW_CHARACTER.sql
@@ -1,5 +1,5 @@
 INSERT INTO characters
-(user_id, class_id, character_name, level, experience, gold, retired, items)
+(user_id, class_id, character_name, level, experience, gold, retired, items, checkmarks, perks)
 VALUES
 ($1, $2, $3, 1, 0, 30, false, '{
     "Head":"",
@@ -8,4 +8,23 @@ VALUES
     "Two Hands": "",
     "Legs":"",
     "Small Item":""
-}');
+}', '{
+    "check1": "false",
+    "check2": "false",
+    "check3": "false",
+    "check4": "false",
+    "check5": "false",
+    "check6": "false",
+    "check7": "false",
+    "check8": "false",
+    "check9": "false",
+    "check10": "false",
+    "check11": "false",
+    "check12": "false",
+    "check13": "false",
+    "check14": "false",
+    "check15": "false",
+    "check16": "false",
+    "check17": "false",
+    "check18": "false"
+}', array[]::text[])
