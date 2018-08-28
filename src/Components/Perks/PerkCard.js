43d6f359347c37c21c726f7f4c24c7f97f625873
reaganforonda <foronda.reagan@gmail.com>
2018-08-27 21:09:01 -0600

    added perk card

---
+++ src/Components/Perks/PerkCard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import BrutePerks from './BrutePerks';
+
+export default function PerkCard(props ) {
+    console.log(~~props.classID);
+
+    let classID  = ~~props.classID;
+    let perkCard = ''
+    switch(classID) {
+        case 1:
+            perkCard = <BrutePerks/>
+            break;
+        case 2:
+            classID = 2
+            break;
+        case 3: 
+            classID = 3
+            break;
+        case 4:
+            classID = 4
+            break;
+        case 5:
+            classID = 5
+            break;
+        case 6:
+            classID = 6
+            break;
+        default:
+            break;
+    }
+
+    console.log(perkCard);
+    
+    return (
+            <div>
+                {perkCard}
+            </div>
+    )
+}
