4f3f57c20d507c9126b72315ff495f81256db36e
reaganforonda <foronda.reagan@gmail.com>
2018-08-27 19:44:19 -0600

    modified create new character sql

--- db/CREATE_NEW_CHARACTER.sql
+++ db/CREATE_NEW_CHARACTER.sql
@@ -8,23 +8,4 @@ VALUES
     "Two Hands": "",
     "Legs":"",
     "Small Item":""
-}', '{
-    "check1": "false",
-    "check2": "false",
-    "check3": "false",
-    "check4": "false",
-    "check5": "false",
-    "check6": "false",
-    "check7": "false",
-    "check8": "false",
-    "check9": "false",
-    "check10": "false",
-    "check11": "false",
-    "check12": "false",
-    "check13": "false",
-    "check14": "false",
-    "check15": "false",
-    "check16": "false",
-    "check17": "false",
-    "check18": "false"
-}', array[]::text[])
+}', $4, $5)
